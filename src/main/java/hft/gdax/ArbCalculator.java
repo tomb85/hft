c92735f2dcb626762c36b0665b4b9ca281f13a1d
Tom <tom.b1985@yahoo.co.uk>
2017-07-15 21:38:57 +0100

    Group arb calculation files by date and session id

--- src/main/java/hft/gdax/ArbCalculator.java
+++ src/main/java/hft/gdax/ArbCalculator.java
@@ -3,38 +3,41 @@ package hft.gdax;
 import com.google.common.collect.Maps;
 import hft.OrderBookListener;
 import hft.Tick;
+import org.joda.time.LocalDate;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.EnumSet;
 import java.util.Map;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static hft.gdax.Product.*;
 
 public class ArbCalculator implements OrderBookListener {
 
     private final Map<Product, Tick> ticks = Maps.newHashMapWithExpectedSize(3);
     private final EnumSet<Product> requiredSymbols = EnumSet.of(BTC_EUR, ETH_BTC, ETH_EUR);
-    private final BufferedWriter writer;
 
     private String sessionId;
+    private BufferedWriter writer;
     private double currentArb;
 
     private static final double THRESHOLD = 0.0000_0000_0000_0001;
 
-    public ArbCalculator() {
-        writer = checkNotNull(getWriter());
-    }
-
     private BufferedWriter getWriter() {
         try {
-            String outputFile = "eur_btc_eth_eur.arb";
-            Files.deleteIfExists(Paths.get(outputFile));
-            Files.createFile(Paths.get(outputFile));
+            if (writer != null) {
+                writer.close();
+            }
+            String outputFile = "output/" + LocalDate.now().toString() + "/eur_btc_eth_eur_" + sessionId + ".arb";
+            Path path = Paths.get(outputFile);
+            if (Files.notExists(path)) {
+                Files.createDirectories(path.getParent());
+                Files.createFile(path);
+            }
             return new BufferedWriter(new FileWriter(outputFile, true));
         } catch (IOException e) {
             return null;
@@ -47,6 +50,7 @@ public class ArbCalculator implements OrderBookListener {
             ticks.clear();
             sessionId = tick.getSessionId();
             currentArb = 0;
+            writer = getWriter();
         }
         Product product = Product.fromProductId(tick.getSymbol());
         ticks.put(product, tick);
@@ -55,7 +59,7 @@ public class ArbCalculator implements OrderBookListener {
             double diff = currentArb - arb;
             currentArb = arb;
             boolean same = diff >= -THRESHOLD && diff <= THRESHOLD;
-            if (!same) {
+            if (!same && writer != null) {
                 try {
                     writer.write(String.join(",", tick.getSessionId(), String.valueOf(tick.getTime()), String.valueOf(arb)));
                     writer.write("\n");
